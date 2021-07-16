3603c09df0ce85c9ae1cc5c9eecad560798697a6
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-07-16 13:51:06 +0200

    layout strony

--- components/header.js
+++ components/header.js
@@ -1,13 +1,12 @@
 import Image from "next/image"
-import swag from "../public/favicon.ico"
 
 export default function Header() {
     return (
-        <nav className="bg-gray-800">
-            <ul>
-                <Image src={swag} alt="" width="30" height="30"/>
-                <h1 className="text-lg">sovietufo.tk</h1>
-                <h2 className="text-base">the most swag website on the internet</h2>
+        <nav className="bg-gray-800 cursor-default">
+            <ul className="flex flex-row text-white space-x-4 p-2 content-center">
+                <Image src="/favicon.ico" alt="" width="40" height="40"/>
+                <span className="text-2xl font-sans" title="the swaggest website on the internet">sovietufo.tk</span>
+                
             </ul>
         </nav>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <h1 className="text-red-900 text-center">sram</h1>
+    <Layout>
+        <h1 className="text-red-900 text-center">sram</h1>
+    </Layout>
   )
 }
