7b4e438010f0498e7d3f13a1e554a1e848079a90
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-07-16 18:30:51 +0200

    navbar teraz jest swag

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 absolute inset-x-0 bottom-0 p-4 font-sans text-white">
+        <footer className="bg-gray-900 absolute inset-x-0 bottom-0 p-4 font-sans text-white">
             &copy; 2021 Soviet Ufo. All rights reserved.
         </footer>
     )
--- components/header.js
+++ components/header.js
@@ -1,13 +1,16 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Header() {
     return (
-        <nav className="bg-gray-800 cursor-default">
-            <ul className="flex flex-row text-white space-x-4 p-2 content-center">
+        <nav className="bg-gray-900 cursor-default font-sans">
+            <div className="flex flex-row text-white space-x-4 p-2 content-center">
                 <Image src="/favicon.ico" alt="" width="40" height="40"/>
                 <span className="text-2xl font-sans" title="the swaggest website on the internet">sovietufo.tk</span>
-                
-            </ul>
+                <Link href="/about" passHref><button className="text-lg px-3 bg-gray-700 hover:bg-gray-800 rounded">About</button></Link>
+                <Link href="/projects" passHref><button className="text-lg px-3 bg-gray-700 hover:bg-gray-800 rounded">Projects</button></Link>
+                <Link href="/contact" passHref><button className="text-lg px-3 bg-gray-700 hover:bg-gray-800 rounded">Contact</button></Link>
+            </div>
         </nav>
     )
 }
