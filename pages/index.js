92385bff37c1c0337d7d2bc85563f3cd0ff66b3c
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-07-20 13:20:56 +0200

    flopa

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Image from 'next/image'
 import Layout from '../components/layout'
 
 export default function Home() {
-  return (
-    <Layout>
-        <h1>Hi!</h1>
-    </Layout>
+    return (
+        <Layout>
+            <h1 className="mb-20">the swaggest fucking site on the internet</h1>
+            <div className="flex justify-center mx-4">
+                <Image src="/images/floppa.jpg" alt="" height="300" width="1000" className="rounded"/>
+            </div>
+        </Layout>
   )
 }
