e1d425d821d977fbec7b78ca8eb4470d126cab49
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-07-27 16:19:40 +0200

    flopp a

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,8 @@ export default function About() {
             <div className="flex justify-center mx-4 mb-4">
                 <Image src="/images/cta.jpg" width="150" height="150" alt="" className="rounded-lg"/>
             </div>
-            <p>i like doing dumb shit</p>
+            <h1 className="mb-4">i like doing dumb shit</h1>
+            <p>and big floppa</p>
         </Layout>
     )
 }
