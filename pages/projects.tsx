3834ac485f42a7d021e54dab81ff020480d9b552
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-08-11 15:50:22 +0200

    cooler projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,7 @@
 export default function Projects() {
     return (
         <>
-            <h1 className="mb-4">Here are some <b className="text-red-600">VERY</b> cool things I made:</h1>
-            <ul className="mb-10">
-                <li><a href="https://github.com/SovietUfo/sovietufo-tk">This site</a></li>
-                
-            </ul>
-            <p>currently nothing else but i will definetely do something cool in the future</p>
+            <h1>you can find all the cool stuff i did on my <a href="https://github.com/SovietUfo?tab=repositories" className="text-blue-400">GitHub profile.</a></h1>
         </>
     )
 }
