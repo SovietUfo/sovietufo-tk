2c45151a3f74826db436b4d1215685ae694727c8
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-08-11 12:26:13 +0200

    500 error page and better 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link"
+
 export default function FortyFour() {
     return (
-        <h1 className="font-mono text-6xl">404 - no page here</h1>
+        <>
+            <h1 className="font-mono text-6xl mb-4">404 - no page here</h1>
+            <div className="justify-center flex"><Link href="/"><a className="text-white text-2xl">return to home</a></Link></div>
+        </>
     )
 }
---
+++ pages/500.tsx
@@ -0,0 +1,3 @@
+export default function FiveHundred() {
+    return <h1 className="font-mono text-6xl">500 - a server-side error occured</h1>
+}
