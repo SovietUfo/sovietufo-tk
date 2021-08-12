0f1f054f0a9891dbc0aa519363cc8cf0011ad392
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-08-12 13:34:55 +0200

    react hooks are fun

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,3 +1,10 @@
+import {useEffect} from "react"
+
 export default function FiveHundred() {
+
+    useEffect(() => {
+        document.title = "a server-side error occured"
+    })
+
     return <h1 className="font-mono text-6xl">500 - a server-side error occured</h1>
 }
