0cf77c0c96bb6809ceb7186765fa9a118c69f6cd
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-08-13 21:55:40 +0200

    brand colors

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -10,11 +10,11 @@ export default function Contact() {
         <>
             <h1>you can find me on:</h1>
             <ul>
-                <li><a href="https://github.com/SovietUfo">GitHub</a></li>
-                <li><a href="https://twitter.com/elSovieto">Twitter</a></li>
-                <li><a href="https://discordapp.com/users/590956498495209474/">Discord</a></li>
-                <li><a href="https://open.spotify.com/user/arcadeaddicted?si=622673af0d9f4862">Spotify</a></li>
-                <li><a href="https://www.last.fm/user/SovietUfo">last.fm</a></li>
+                <li><a href="https://github.com/SovietUfo" className="hover:text-[#333]">GitHub</a></li>
+                <li><a href="https://twitter.com/elSovieto" className="hover:text-[#1DA1F2]">Twitter</a></li>
+                <li><a href="https://discordapp.com/users/590956498495209474/" className="hover:text-[#5865F2]">Discord</a></li>
+                <li><a href="https://open.spotify.com/user/arcadeaddicted?si=622673af0d9f4862" className="hover:text-[#1DB954]">Spotify</a></li>
+                <li><a href="https://www.last.fm/user/SovietUfo" className="hover:text-[#D51007]">last.fm</a></li>
             </ul>
         </>
     )
