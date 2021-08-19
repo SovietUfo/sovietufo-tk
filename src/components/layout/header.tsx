874baff5af1b97cec65213c307bd3532a8257276
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-08-19 17:47:02 +0200

    navbar toggle button

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,18 +1,30 @@
 import Image from "next/image"
 import Link from "next/link"
+import Toggle from "./toggle"
+import {useState} from "react"
 
 export default function Header() {
+
+    const [isHidden, setIsHidden] = useState(false)
+
+    function toggleVisibility() {
+        setIsHidden(!isHidden)
+    }
+
     return (
         <nav>
-            <div className="flex items-center flex-shrink-0 mr-6">
-                <Image src="/favicon.ico" width="40" height="40" alt=""></Image>
-                <Link href="/" passHref><Image src="/images/logo.gif" alt="sovietufo.tk" height="45" width="135" className="cursor-pointer"/></Link>
+            <div className="flex items-center flex-shrink-0 ml-3 lg:ml-0 justify-between">
+                <div>
+                    <Image src="/favicon.ico" width="40" height="40" alt=""></Image>
+                    <Link href="/" passHref><Image src="/images/logo.gif" alt="sovietufo.tk" height="45" width="135" className="cursor-pointer"/></Link>
+                </div>
+                <div onClick={toggleVisibility} className="cursor-pointer"><Toggle /></div>
             </div>
-            <div className="flex-grow"></div>
-            <Link href="/about" passHref><button className="flex-none">About</button></Link>
-            <Link href="/projects" passHref><button className="flex-none">My projects</button></Link>
-            <Link href="/contact" passHref><button className="flex-none">Contact</button></Link>
-            
+            <div className={isHidden ? "lg:space-x-6" : "hidden lg:block lg:space-x-6"}>
+                <Link href="/about" passHref><button className="flex-none">About</button></Link>
+                <Link href="/projects" passHref><button className="flex-none">My projects</button></Link>
+                <Link href="/contact" passHref><button className="flex-none">Contact</button></Link>
+            </div>
         </nav>
     )
 }
---
+++ src/components/layout/toggle.tsx
@@ -0,0 +1,7 @@
+import {BsList} from "react-icons/bs"
+
+export default function Toggle() {
+    return (
+        <div className="lg:hidden h-10 w-10 m-3 rounded"><BsList className="w-full h-full text-white"/></div>
+    )
+}
