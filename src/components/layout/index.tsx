d8b6223d1ad4aeb902fc21b5b4e7b9f3dd6d60b5
soviet ufo <77695657+SovietUfo@users.noreply.github.com>
2021-08-21 20:20:17 +0200

    flex > sex

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,10 +3,10 @@ import Header from "./header"
 
 export default function Layout({children}) {
     return (
-        <>
+        <div className="flex flex-col justify-between h-screen">
             <Header />
             <main>{children}</main>
             <Footer />
-        </>
+        </div>
     )
 }
