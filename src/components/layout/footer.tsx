6cae350da4dce779c949019f0283926a15d4132c
SovietUfo <ommm@interia.pl>
2021-09-07 18:15:11 +0200

    cooler looks

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className="bg-[#1a0936] p-4 text-purple-300 text-center">
             &copy; 2021 Soviet Ufo. All rights reserved.
         </footer>
     )
