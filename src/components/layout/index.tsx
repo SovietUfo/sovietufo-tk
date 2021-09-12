75d0becd01d3eab700f14c8bc83aad5332d5d738
SovietUfo <ommm@interia.pl>
2021-09-12 11:38:14 +0200

    typescript strict mode

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,13 @@
 import Footer from "./footer"
 import Header from "./header"
 
-export default function Layout({children}) {
+import React from "react"
+
+type Props = {
+    children: React.ReactNode
+}
+
+export default function Layout({children}: Props) {
     return (
         <div className="flex flex-col justify-between h-screen w-screen">
             <Header />
