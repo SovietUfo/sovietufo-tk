65cdbb3db00bed58fe77f035b3201c6be783556d
SovietUfo <ommm@interia.pl>
2021-10-23 21:57:31 +0200

    la siesta

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -16,9 +16,10 @@ export default function Contact() {
                 <li><LinkButton url="https://twitter.com/sovietufo" text="twitter" icon={<FaTwitter className="inline mr-1 mb-1"/>}/></li>
                 <li><LinkButton url="https://discordapp.com/users/590956498495209474/" text="discord" icon={<FaDiscord className="inline mr-1 mb-1"/>}/></li>
             </ul>
-            <h1 className="text-2xl">other stuff:</h1>
+            <h1 className="text-3xl">other stuff:</h1>
             <ul>
                 <li><LinkButton url="https://github.com/SovietUfo" text="github" icon={<FaGithub className="inline mr-1 mb-1"/>}/></li>
+                <h1 className="text-xl">Music: </h1>
                 <li><LinkButton url="https://open.spotify.com/user/arcadeaddicted" text="spotify" icon={<FaSpotify className="inline mr-1 mb-1"/>}/></li>
                 <li><LinkButton url="https://last.fm/user/SovietUfo" text="last.fm" icon={<FaLastfm className="inline mr-1 mb-1"/>}/></li>
             </ul>
