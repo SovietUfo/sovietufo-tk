e3290b026d472f4786e94da93f9b9b7f8dda695d
SovietUfo <ommm@interia.pl>
2022-01-02 15:22:24 +0100

    update da year in footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@
 export default function Footer() {
     return (
         <footer className="bg-[#2E3440] p-4 text-[#D8DEE9] text-center z-10">
-            &copy; 2021 Soviet Ufo. All rights reserved.
+            &copy; {new Date().getFullYear()} Soviet Ufo. All rights reserved.
         </footer>
     )
 }
